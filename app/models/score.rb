ffe323f5a371e2d9f772076cd70490ff7da36c38
ikstrm <mail@ikst.jp>
2013-04-27 03:41:46 +0900

    Implement Score

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,5 +1,11 @@
 class Score
-  def update
-    ;
+  def self.update(args = {})
+    return unless args[:iidxid]
+    return unless args[:music]
+    return unless args[:difficulty]
+    return unless args[:value]
+
+    key = "score:#{args[:iidxid]}:#{args[:music]}:#{args[:difficulty]}"
+    $redis.set(key, args[:value]);
   end
 end
--- spec/models/score_spec.rb
+++ spec/models/score_spec.rb
@@ -15,7 +15,7 @@ describe Score do
     context "when score doesn't exist" do
       it "should create score" do
         @redis.get("score:1111-1111:冥:SPA").should == nil
-        Score.create(iidxid: "1111-1111", music: "冥", difficulty: "SPA", value: "4000")
+        Score.update(iidxid: "1111-1111", music: "冥", difficulty: "SPA", value: "4000")
         @redis.get("score:1111-1111:冥:SPA").should == "4000"
       end
     end
@@ -23,7 +23,7 @@ describe Score do
     context "when score exists" do
       it "should update score" do
         @redis.set("score:1111-1111:冥:SPA", "2000")
-        Score.create(iidxid: "1111-1111", music: "冥", difficulty: "SPA", value: "4000")
+        Score.update(iidxid: "1111-1111", music: "冥", difficulty: "SPA", value: "4000")
         @redis.get("score:1111-1111:冥:SPA").should == "4000"
       end
     end
