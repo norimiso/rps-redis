29f1ec3e340775aefc66a2088867d40a00d33eff
ikstrm <mail@ikst.jp>
2013-04-27 12:38:11 +0900

    Make model Power

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User
-  def self.create(iidxid)
-    $redis.sadd("user", iidxid)
+  def self.create(args = {})
+    return unless args[:iidxid]
+    return unless args[:djname]
+
+    $redis.sadd("user", args[:iidxid])
+    $redis.set("user:#{args[:iidxid]}", args[:djname])
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,16 +14,18 @@ describe User do
     context "user not already added" do
       it "should create user" do
         @redis.smembers("user").should_not include "1111-1111"
-        User.create("1111-1111")
+        User.create(iidxid: "1111-1111", djname: "test")
         @redis.smembers("user").should include "1111-1111"
+        @redis.get("user:1111-1111").should == "test"
       end
     end
 
     context "user already added" do
-      it "should do nothing" do
+      it "should update his djname" do
         @redis.sadd("user", "1111-1111")
-        User.create("1111-1111")
-        @redis.smembers("user").should include "1111-1111"
+        @redis.set("user:1111-1111", "test1")
+        User.create(iidxid: "1111-1111", djname: "test2")
+        @redis.get("user:1111-1111").should == "test2"
       end
     end
   end
