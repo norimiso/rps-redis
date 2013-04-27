c8e848cdff7531c4dfff516ae95824f3e8f5af2a
ikstrm <mail@ikst.jp>
2013-04-27 17:30:40 +0900

    Configure APIs

--- config/routes.rb
+++ config/routes.rb
@@ -1,49 +1,13 @@
 Rps::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  root to: 'users#index', as: :index
 
-  # You can have the root of your site routed with "root"
-  root to: 'users#index'
+  post "users/create"   => "users#create",  as: :create_user
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get  "scores/:iidxid" => "scores#show",   as: :show_scores
+  post "scores/update"  => "scores#update", as: :scores_update
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  post "powers/update"  => "powers#update", as: :update_powers
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  get  "musics"         => "musics#index",  as: :musics
+  post "musics/update"  => "musics#update", as: :update_musics
 end
