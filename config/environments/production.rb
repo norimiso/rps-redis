06ef2c0a680e76c9fb432409bd731b19369cabcc
ikstrm <mail@ikst.jp>
2013-04-30 02:04:26 +0900

    Fix application name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Beatech::Application.configure do
+Rps::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
