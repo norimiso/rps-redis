743781a36c4def283a06d3a3247e808baf51d7ed
ikstrm <mail@ikst.jp>
2013-04-30 01:14:47 +0900

    Automatically reconnect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :null_session
+  before_filter :reconnect
+
+  def reconnect
+    begin
+      $redis.set("connection check", "foo")
+    rescue
+      $redis.client.reconnect
+    end
+  end
 end
