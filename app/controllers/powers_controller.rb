a957951a2e56a947b91910006975b3ce04ea688f
ikstrm <mail@ikst.jp>
2013-05-01 07:39:24 +0900

    Add test

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,2 +1,17 @@
 class PowersController < ApplicationController
+  def update
+    Power.update(
+      iidxid: "1111-1111",
+      sp8s: "1",
+      sp9s: "1",
+      sp10s: "1",
+      sp11s: "1",
+      sp12s: "1",
+      sps: "1",
+      sp11c: "1",
+      sp12c: "1",
+      spc: "1"
+    )
+    render text: "update succeeded"
+  end
 end
--- spec/controllers/powers_controller_spec.rb
+++ spec/controllers/powers_controller_spec.rb
@@ -1,5 +1,32 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe PowersController do
+  describe "POST 'scores/update'" do
+    before do
+      @redis = Redis.new(host: 'localhost', port: 6379)
+      @redis.flushall
+      User.create(iidxid: "1111-1111", djname: "test")
+      @score = Score.new(iidxid: "1111-1111", title: "冥", playtype: "SP", difficulty: "A")
+      @score.update(exscore: "3000", bp: "10", clear: "EH")
+      xhr :post, :update, iidxid: "1111-1111"
+    end
 
+    it "should be success" do
+      response.should be_success
+    end
+
+    it "should change power values" do
+      @power = Power.find(iidxid: "1111-1111")
+      @power[:sp8s ].should_not == nil
+      @power[:sp9s ].should_not == nil
+      @power[:sp10s].should_not == nil
+      @power[:sp11s].should_not == nil
+      @power[:sp12s].should_not == nil
+      @power[:sps  ].should_not == nil
+      @power[:sp11c].should_not == nil
+      @power[:sp12c].should_not == nil
+      @power[:spc  ].should_not == nil
+    end
+  end
 end
